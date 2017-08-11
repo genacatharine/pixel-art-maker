06fac0896d3ea4e3777eefeeab0c64d5098e87b2
Gena Israel <gena.catharine@gmail.com>
2017-08-11 16:06:04 -0600

    first commit

---
+++ pixelartjs.js
@@ -0,0 +1,59 @@
+// function createPixel(text){
+//
+
+// let pixel=document.createElement('pixel')
+// pixel.innerHTML = text;
+// div.appendChild(pixel);
+// }
+//
+//
+
+// let button= document.getElementById('red-button');
+// button.addEventListener("click", function() {
+//   console.log("you click the button!!", button)
+//   createDiv("bill's excellent")
+//
+//   })
+// })
+
+// var container=document.createElement("div");
+// container.setAttribute("class", "container");
+//
+// var colorSelector= document.createElement("div");
+// colorSelector.setAttribute("class", "colorSelector");
+
+document.addEventListener("DOMContentLoaded", function () {
+  function addDiv() {
+    var container = document.getElementById("container");
+
+    for (var i = 0; i < 100; i++) {
+      var id = `pixel-${i}`;
+      var pixel= document.createElement("div");
+      pixel.setAttribute("id", id);
+      pixel.setAttribute("style", "width: 10px; height: 10px; border: 1px solid black;");
+      container.appendChild(pixel);
+    }
+  }
+
+  addDiv();
+});
+
+// pixel.style.float="left";
+// pixel.style.border="1px solid black";
+// pixel.style.width="15px";
+// pixel.style.height="15px";
+// }
+// })
+
+
+//   toAdd.appendChild(newDiv);
+// }
+// document.appendChild(toAdd);
+// }
+
+//   img.addEventListener('click' imgLog);
+// });
+
+// body
+// div with colors
+// div with pixels
